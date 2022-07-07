ff267b235c696be3ab61fda7b5e3a78933f4a58d
gaaiiinnnn <g3gan93@gmail.com>
2022-07-07 13:21:39 +0900

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+new fullpage('#fullpage', {
+	//options here
+	autoScrolling:true,
+	scrollHorizontally: true,
+    scrollBar:true,
+    sectionsColor: ['#95AAA3', '#EBE7E4', '#7E8F7C', '#FAFAD2' ],
+    navigation: true,
+	navigationPosition: 'right',
+    navigationTooltips: ['GHA소개', '제다큐어', '연구개발', '본사위치', '연구소위치'],
+    easing: 'easeInOutCubic',
+    menu: '.gnb_list',
+    anchors: ['s1', 's2', 's3', 's4', 's5'],
+});
