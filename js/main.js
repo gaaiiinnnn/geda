515ab81683f0732ef1387cd0e7207930aefafe06
gaaiiinnnn <107171411+gaaiiinnnn@users.noreply.github.com>
2022-07-15 12:22:50 +0900

    7/15 메뉴

--- js/main.js
+++ js/main.js
@@ -12,3 +12,25 @@ new fullpage('#fullpage', {
     anchors: ['1st', '2nd', '3rd', '4th', '5th'],
     normalScrollElements: '#s2',
 });
+
+var myScroll = new IScroll('.total_menu', {
+    mouseWheel: true,
+    //scrollbars: true
+});
+
+$('.total_mitem').click(function(){
+    if($(this).hasClass('active')){ //현재 active가 적용된 상태일 때
+        $(this).removeClass('active');
+    } else {
+        $('.total_mitem').removeClass('active');
+        $(this).addClass('active');
+    }
+    
+});
+
+$('.totalmenu_w').click(function(){ //menu 부분 누르면
+    $('.total_menu_z').addClass('active');
+});
+$('.totalClose_w').click(function(){
+    $('.total_menu_z').removeClass('active');
+});
